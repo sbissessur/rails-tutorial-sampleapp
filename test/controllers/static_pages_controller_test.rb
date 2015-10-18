e5a20c748cde73b21bbb65ce51ba84b841f24659
sbissessur <sahilbissessur@umail.ucsb.edu>
2015-10-18 23:10:07 +0000

    Add about action

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  # What this test says:
+  # "Let's test the Home page by issuing a GET request to the home action and then making sure
+  # we receive a 'success' status code in response"
+  
   test "should get home" do
+    # accessed using a GET request
     get :home
     assert_response :success
   end
@@ -11,4 +16,9 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+  
 end

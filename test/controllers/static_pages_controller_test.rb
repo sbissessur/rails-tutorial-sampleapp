3b440030912d64b3b150e3e5063fd5c9d3caaa1a
sbissessur <sahilbissessur@umail.ucsb.edu>
2015-10-18 23:23:02 +0000

    Update view for Home page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,16 +9,20 @@ class StaticPagesControllerTest < ActionController::TestCase
     # accessed using a GET request
     get :home
     assert_response :success
+    # assert_select lets us test for the presence of a particular HTML tag
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
   
 end
